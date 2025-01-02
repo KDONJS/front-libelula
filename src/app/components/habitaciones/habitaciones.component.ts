77fadc1f49cd8febf2fbf90a111e894c09e0ea5b
KDONJS <yorlinssj@hotmail.com>
2025-01-01 21:04:48 -0500

    add Habitaciones component with hero section, detailed description, and responsive styles

--- src/app/components/habitaciones/habitaciones.component.ts
+++ src/app/components/habitaciones/habitaciones.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../utils/hero/hero.component';
 
 @Component({
   selector: 'app-habitaciones',
   standalone: true,
-  imports: [],
+  imports: [
+    HeroComponent
+  ],
   templateUrl: './habitaciones.component.html',
   styleUrl: './habitaciones.component.css'
 })
 export class HabitacionesComponent {
-
+  hero = {
+    "mensaje": "Reserva tu estancia y disfruta del lujo redefinido a las tarifas más asequibles.",
+    "titulo": "Habitaciones"
+  }
 }
