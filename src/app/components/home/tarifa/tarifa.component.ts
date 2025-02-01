4ad36e3f15c5c1a75f7bb43a84be09fcef0371f2
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2025-02-01 10:38:24 -0500

    refactor components to use dynamic data for titles and content, update styles for responsiveness

--- src/app/components/home/tarifa/tarifa.component.ts
+++ src/app/components/home/tarifa/tarifa.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { PocketbaseService } from '../../../services/pocketbase.service';
+import { CommonModule } from '@angular/common';
+
+interface Razones {
+  collectionId: string;
+  collectionName: string;
+  created: string;
+  id: string;
+  name: string;
+  titulo: string;
+  contenido: string;
+  imagen: number;
+  updated: string;
+}
 
 @Component({
   selector: 'app-tarifa',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './tarifa.component.html',
   styleUrl: './tarifa.component.css'
 })
 export class TarifaComponent {
+  titulo: string = 'tarifa';
+  collectionName: string = 'razones';
 
+  razones?: Razones | null = null;
+
+  constructor(private pocketBaseService: PocketbaseService) {}
+
+  ngOnInit(): void {
+    this.getHeroData();
+  }
+
+  private getHeroData(): void {
+    this.pocketBaseService.getCollection(this.collectionName).subscribe({
+      next: (response) => {
+        this.razones = response.items.find(
+          (item: Razones) => item.name === this.titulo
+        ) || null; // Si no encuentra nada, asigna `null`
+      },
+      error: (error) => {
+        console.error('Error obteniendo datos:', error);
+        this.razones = null; // Manejar el error y evitar undefined
+      },
+    });
+  }
 }
