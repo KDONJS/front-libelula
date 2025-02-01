bb874f598e9fd109a15ef039aa3595923d94c3f6
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2025-02-01 16:37:45 -0500

    refactor Habitaciones component to use dynamic data for room details and improve layout styles

--- src/app/components/habitaciones/habitaciones.component.ts
+++ src/app/components/habitaciones/habitaciones.component.ts
@@ -1,18 +1,56 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../utils/hero/hero.component';
+import { PocketbaseService } from '../../services/pocketbase.service';
+import { CommonModule } from '@angular/common';
+
+interface Habitacion {
+  collectionId: string;
+  collectionName: string;
+  created: string;
+  id: string;
+  imagenes: string[];
+  description: string;
+  numero_celular: string;
+  tipo: number;
+  updated: string;
+}
 
 @Component({
   selector: 'app-habitaciones',
   standalone: true,
   imports: [
-    HeroComponent
+    HeroComponent,
+    CommonModule
   ],
   templateUrl: './habitaciones.component.html',
   styleUrl: './habitaciones.component.css'
 })
 export class HabitacionesComponent {
   hero = {
-    "mensaje": "Reserva tu estancia y disfruta del lujo redefinido a las tarifas más asequibles.",
+    "mensaje": "",
     "titulo": "Habitaciones"
   }
+
+  collectionName: string = 'habitaciones';
+
+  habitaciones: Habitacion[] = []; // Inicializar como un array vacío
+
+  constructor(private pocketBaseService: PocketbaseService) {}
+
+  ngOnInit(): void {
+    this.getHeroData();
+  }
+
+  private getHeroData(): void {
+    this.pocketBaseService.getCollection(this.collectionName).subscribe({
+      next: (response) => {
+        this.habitaciones = response.items as Habitacion[]; // Asegurar el tipo correcto
+      },
+      error: (error) => {
+        console.error('Error obteniendo datos:', error);
+        this.habitaciones = [];
+      },
+    });
+  }
+
 }
