84b8c1c19f702315a33d3eca9f3e18e6cee25cdb
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2025-02-02 10:14:33 -0500

    fetch and display installations data dynamically in Instalaciones component

--- src/app/components/instalaciones/instalaciones.component.ts
+++ src/app/components/instalaciones/instalaciones.component.ts
@@ -1,18 +1,54 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../utils/hero/hero.component";
+import { PocketbaseService } from '../../services/pocketbase.service';
+import { CommonModule } from '@angular/common';
+
+interface Instalacion {
+  collectionId: string;
+  collectionName: string;
+  created: string;
+  id: string;
+  instalacion: string;
+  imagen: string;
+  detalle: string;
+  updated: string;
+}
 
 @Component({
   selector: 'app-instalaciones',
   standalone: true,
   imports: [
     HeroComponent,
+    CommonModule
   ],
   templateUrl: './instalaciones.component.html',
   styleUrl: './instalaciones.component.css'
 })
 export class InstalacionesComponent {
   hero = {
-    "mensaje": "Disfruta de nuestras instalaciones de lujo.",
+    "mensaje": "",
     "titulo": "Instalaciones"
+  }
+
+  collectionName: string = 'instalaciones';
+
+  instalaciones: Instalacion[] = []; // Inicializar como un array vacío
+
+  constructor(private pocketBaseService: PocketbaseService) {}
+
+  ngOnInit(): void {
+    this.getHeroData();
+  }
+
+  private getHeroData(): void {
+    this.pocketBaseService.getCollection(this.collectionName).subscribe({
+      next: (response) => {
+        this.instalaciones = response.items as Instalacion[]; // Asegurar el tipo correcto
+      },
+      error: (error) => {
+        console.error('Error obteniendo datos:', error);
+        this.instalaciones = [];
+      },
+    });
   }
 }
