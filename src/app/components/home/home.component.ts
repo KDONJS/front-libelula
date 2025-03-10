fbf9f5a627966e92eb66e14abffeb827a8d500de
KDONJS <yorlinssj@hotmail.com>
2025-03-09 22:45:26 -0500

    feat: implement customer comments carousel in home component with styling and data integration

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone, ChangeDetectorRef } from '@angular/core';
 import { HeroComponent } from "../utils/hero/hero.component";
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule, NgFor } from '@angular/common';
+import { PocketbaseService } from '../../services/pocketbase.service';
+
+
+
+interface comentario{
+  collectionId: string,
+  collectionName: string,
+  id: string,
+  autor: string,
+  comentario: string,
+  fecha: Date,
+  estrellas: Number,
+  pais: string,
+  created: string,
+  updated: string
+}
 
 @Component({
   selector: 'app-home',
@@ -9,14 +26,72 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     HeroComponent,
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    CommonModule,
+    NgFor
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  currentIndex: number = 0;
+  autoplayInterval: any;
+  collectionName: string = 'comentarios_estadia';
+  comentarios: comentario[] = [];
+  estrellas: number = 0;
+
   hero = {
     "mensaje": "",
     "titulo": "Bienvenidos"
+  };
+
+  constructor(
+    private ngZone: NgZone, 
+    private cdr: ChangeDetectorRef,
+    private pocketBaseService: PocketbaseService
+  ) {}
+
+  ngOnInit() {
+    this.getComentariosData();
+  }
+
+  private getComentariosData(): void {
+    this.pocketBaseService.getCollection(this.collectionName).subscribe({
+      next: (response) => {
+        this.comentarios = response.items as comentario[];
+        this.estrellas = this.comentarios.reduce((acc, item) => acc + Number(item.estrellas), 0) / this.comentarios.length;
+        this.startAutoplay();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.stopAutoplay();
+  }
+
+  startAutoplay() {
+    this.ngZone.runOutsideAngular(() => {
+      this.autoplayInterval = setInterval(() => {
+        this.ngZone.run(() => {
+          this.nextComment();
+          this.cdr.markForCheck();
+        });
+      }, 3000);
+    });
+  }
+
+  stopAutoplay() {
+    if (this.autoplayInterval) {
+      clearInterval(this.autoplayInterval);
+    }
+  }
+
+  prevComment() {
+    this.currentIndex = (this.currentIndex - 1 + this.comentarios.length) % this.comentarios.length;
+  }
+
+  nextComment() {
+    this.currentIndex = (this.currentIndex + 1) % this.comentarios.length;
   }
 }
